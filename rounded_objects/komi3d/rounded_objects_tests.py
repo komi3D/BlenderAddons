0acab477542fb16e7bfafb64d657374e4948ceef
komi3D <komi3dmail@gmail.com>
2015-06-30 00:14:22 +0200

    Large overlap UTs added

--- rounded_objects/komi3d/rounded_objects_tests.py
+++ rounded_objects/komi3d/rounded_objects_tests.py
@@ -19,6 +19,77 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
+    # ## 2 intersections
+
+    def testACIForTwoIntersectionsSmallOverlap(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 1.0
+        c2 = Vector((4, 0, 0))
+        r2 = 3.5
+        intersection = [2, 4]
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        # self.assertEqual(2, len(output))
+        self.assertEqual(intersection, output)
+
+    def testACIForTwoIntersectionsLargeOverlapPlusX(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 6.0
+        c2 = Vector((4, 0, 0))
+        r2 = 2.5
+        intersection1 = Vector((5.71875, 1.8155, 0.0))
+        intersection2 = Vector((5.71875, -1.8155, 0.0))
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(2, len(output))
+        self.assertAlmostEqual(intersection1[0], output[0][0], places = 3)
+        self.assertAlmostEqual(intersection1[1], output[0][1], places = 3)
+        self.assertAlmostEqual(intersection1[2], output[0][2], places = 3)
+
+        self.assertAlmostEqual(intersection2[0], output[1][0], places = 3)
+        self.assertAlmostEqual(intersection2[1], output[1][1], places = 3)
+        self.assertAlmostEqual(intersection2[2], output[1][2], places = 3)
+
+    def testACIForTwoIntersectionsLargeOverlapMinusX(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 6.0
+        c2 = Vector((-4, 0, 0))
+        r2 = 2.5
+        intersection1 = Vector((-5.71875, -1.8155, 0.0))
+        intersection2 = Vector((-5.71875, 1.8155, 0.0))
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(2, len(output))
+        self.assertAlmostEqual(intersection1[0], output[0][0], places = 3)
+        self.assertAlmostEqual(intersection1[1], output[0][1], places = 3)
+        self.assertAlmostEqual(intersection1[2], output[0][2], places = 3)
+
+        self.assertAlmostEqual(intersection2[0], output[1][0], places = 3)
+        self.assertAlmostEqual(intersection2[1], output[1][1], places = 3)
+        self.assertAlmostEqual(intersection2[2], output[1][2], places = 3)
+
+    def testACIForTwoIntersectionsLargeOverlapMinusY(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 6.0
+        c2 = Vector((0, -4, 0))
+        r2 = 2.5
+        intersection1 = Vector((1.8155, -5.71875, 0.0))
+        intersection2 = Vector((-1.8155, -5.71875, 0.0))
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(2, len(output))
+        self.assertAlmostEqual(intersection1[0], output[0][0], places = 3)
+        self.assertAlmostEqual(intersection1[1], output[0][1], places = 3)
+        self.assertAlmostEqual(intersection1[2], output[0][2], places = 3)
+
+        self.assertAlmostEqual(intersection2[0], output[1][0], places = 3)
+        self.assertAlmostEqual(intersection2[1], output[1][1], places = 3)
+        self.assertAlmostEqual(intersection2[2], output[1][2], places = 3)
+
     ### Tangency tests ###
     def testCIForTangentCirclesWithEqualRadiusReturnOnePoint2(self):
         c1 = Vector((0, 0, 0))
@@ -105,33 +176,7 @@ class Test(unittest.TestCase):
 
         self.assertEqual(intersection, output)
 
-    # ## 2 intersections
 
-    def testCIForTwoIntersectionsSmallOverlap(self):
-        c1 = Vector((0, 0, 0))
-        r1 = 1.0
-        c2 = Vector((4, 0, 0))
-        r2 = 3.5
-        intersection = [2, 4]
-
-        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
-
-        # self.assertEqual(2, len(output))
-        self.assertEqual(intersection, output)
-
-    def testCIForTwoIntersectionsLargeOverlap(self):
-        c1 = Vector((0, 0, 0))
-        r1 = 6.0
-        c2 = Vector((4, 0, 0))
-        r2 = 2.5
-        intersection1 = [5.5, 2.0]
-        intersection2 = [5.5, -2.0]
-
-        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
-
-        self.assertEqual(2, len(output))
-        self.assertEqual(intersection1, output[0])
-        self.assertEqual(intersection2, output[1])
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
