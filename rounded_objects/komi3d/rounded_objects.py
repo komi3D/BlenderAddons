47a63d4a0f95f4ad8eb61572177e4a9acf83cf76
komi3D <komi3dmail@gmail.com>
2015-06-29 23:39:25 +0200

    calculating 2 circle intersection - large overlap

--- rounded_objects/komi3d/rounded_objects.py
+++ rounded_objects/komi3d/rounded_objects.py
@@ -3,6 +3,10 @@ Created on 28 cze 2015
 
 @author: Komi
 '''
+from math import fabs, sqrt
+
+from mathutils import Vector
+
 
 class RoundedObjects(object):
     '''
@@ -16,6 +20,55 @@ class RoundedObjects(object):
         '''
         pass
 
+
+    def getCenterVecAndLength(self, c1, c2):
+        center_vec = c2 - c1
+        center_vec_len = center_vec.length
+        return (center_vec, center_vec_len)
+
     def getCircleIntersections(self, center1, r1, center2, r2):
 
-        return None
+        center_vec, center_vec_len = self.getCenterVecAndLength(center1, center2)
+        vec = None
+        sumOfRadius = r1 + r2
+        differenceOfRadius = fabs(r1 - r2)
+
+        if (sumOfRadius == center_vec_len):  # one intersection
+            vec = center1 + center_vec * r1 / center_vec_len
+            return [vec]
+        elif (differenceOfRadius == center_vec_len):  # one intersection
+            vec = center1 - center_vec * r1 / center_vec_len
+            return [vec]
+        elif ((sumOfRadius > center_vec_len) and (differenceOfRadius > center_vec_len)) or (sumOfRadius < center_vec_len):  # none intersections:
+            return None
+        elif (sumOfRadius > center_vec_len) and (differenceOfRadius < center_vec_len):  # two intersections
+            return self.calculateTwoIntersections(center1, r1, center2, r2)
+        else:
+            return 333
+
+    def calculateTwoIntersections(self, center1, r1, center2, r2):
+        center_vec, center_vec_len = self.getCenterVecAndLength(center1, center2)
+        if r1 > center_vec_len or r2 > center_vec_len :
+            return self.calculateCircleIntersectionWithLargeOverlap(center1, r1, center2, r2)
+        else:
+            return self.calculateCircleIntersectionWithSmallOverlap(center1, r1, center2, r2)
+
+    def calculateCircleIntersectionWithLargeOverlap(self, center1, r1, center2, r2):
+        #
+        center_vec, center_vec_len = self.getCenterVecAndLength(center1, center2)
+        A = center_vec_len
+        x = ((A ** 2 + r2 ** 2) - r1 ** 2) / (-2 * A)
+        h = sqrt(r2 ** 2 - x ** 2)
+
+        intersectionX = center2 + center_vec * (x / A)
+        perpendicularVec = Vector((-center_vec[1], center_vec[0], center_vec[2]))  # TODO calculate perpendicular based on selected plane
+        perpendicularVecLen = perpendicularVec.length
+        intersection1 = intersectionX + perpendicularVec * (h / perpendicularVecLen)
+        intersection2 = intersectionX - perpendicularVec * (h / perpendicularVecLen)
+
+        return [intersection1, intersection2]
+
+
+    def calculateCircleIntersectionWithSmallOverlap(self, center1, r1, center2, r2):
+        center_vec, center_vec_len = self.getCenterVecAndLength(center1, center2)
+        return 222
--- rounded_objects/komi3d/rounded_objects_tests.py
+++ rounded_objects/komi3d/rounded_objects_tests.py
@@ -19,21 +19,8 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
-
-    def testVectorAdding(self):
-        v1 = Vector((2, 3, 0))
-        v2 = Vector((2, 3, 0))
-        result = Vector((4, 6, 0))
-
-        self.assertEqual(result, v1 + v2)
-        
-    def testVectorLength(self):
-        v1 = Vector((1, 1, 0))
-        length = v1.length
-        
-        self.assertEqual(sqrt(2), length)
-        
-    def test1(self):
+    ### Tangency tests ###
+    def testCIForTangentCirclesWithEqualRadiusReturnOnePoint2(self):
         c1 = Vector((0, 0, 0))
         r1 = 2.0
         c2 = Vector((4, 0, 0))
@@ -42,8 +29,109 @@ class Test(unittest.TestCase):
 
         output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
 
+        self.assertEqual(1, len(output))
+        self.assertEqual(intersection, output[0])
+
+    def testCIForTangentCirclesWithEqualRadiusReturnOnePoint3(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 3.0
+        c2 = Vector((6, 0, 0))
+        r2 = 3.0
+        intersection = Vector((3, 0, 0))
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(1, len(output))
+        self.assertEqual(intersection, output[0])
+
+    def testCIForTangentCirclesAlongXReturnOnePoint(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 2.0
+        c2 = Vector((6, 0, 0))
+        r2 = 4.0
+        intersection = Vector((2, 0, 0))
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(1, len(output))
+        self.assertEqual(intersection, output[0])
+
+    def testCIForInnerTangentCirclesAlongXReturnOnePoint(self):
+        c1 = Vector((3, 0, 0))
+        r1 = 1.0
+        c2 = Vector((6, 0, 0))
+        r2 = 4.0
+        intersection = Vector((2, 0, 0))
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(1, len(output))
+        self.assertEqual(intersection, output[0])
+
+    def testCIForTangentCirclesAlongYReturnOnePoint(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 2.0
+        c2 = Vector((0, 6, 0))
+        r2 = 4.0
+        intersection = Vector((0, 2, 0))
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(1, len(output))
+        self.assertEqual(intersection, output[0])
+    ###################################################
+
+    # ## no intersection
+
+    def testCIForNoIntersectionsWhenCenterDistanceLargerThenRadiusesSum(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 2.0
+        c2 = Vector((10, 0, 0))
+        r2 = 4.0
+        intersection = None
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
         self.assertEqual(intersection, output)
 
+    def testCIForNoIntersectionsWhenCircleInsideTheOtherCircle(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 20.0
+        c2 = Vector((10, 0, 0))
+        r2 = 4.0
+        intersection = None
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(intersection, output)
+
+    # ## 2 intersections
+
+    def testCIForTwoIntersectionsSmallOverlap(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 1.0
+        c2 = Vector((4, 0, 0))
+        r2 = 3.5
+        intersection = [2, 4]
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        # self.assertEqual(2, len(output))
+        self.assertEqual(intersection, output)
+
+    def testCIForTwoIntersectionsLargeOverlap(self):
+        c1 = Vector((0, 0, 0))
+        r1 = 6.0
+        c2 = Vector((4, 0, 0))
+        r2 = 2.5
+        intersection1 = [5.5, 2.0]
+        intersection2 = [5.5, -2.0]
+
+        output = self.roundedObj.getCircleIntersections(c1, r1, c2, r2)
+
+        self.assertEqual(2, len(output))
+        self.assertEqual(intersection1, output[0])
+        self.assertEqual(intersection2, output[1])
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
